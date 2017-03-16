a972c7620cf642abb0769048e4ff209dd973eb6b
qvolant <quentin.volant@gmail.com>
2017-03-16 16:56:11 +0100

    Séparation header/footer + navbar

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="css/materialize.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/main.min.css" rel="stylesheet">
-    <title>Rivages Propres</title>
-</head>
-<body>
+<?php
+include_once 'includes/header.inc.php';
+include_once 'includes/navbar.inc.php';
+?>
 
 
 
-<script src="js/jquery.min.js" type="text/javascript"></script>
-<script src="js/materialize.min.js" type="text/javascript"></script>
-<script src="js/main.min.js" type="text/javascript"></script>
-</body>
-</html>
+<?php
+include_once 'includes/footer.inc.php';
+?>
