9f1fd8bac170a264c883982472e75d8ac2ca0933
Provain <thomas.provain@gmail.com>
2017-03-21 15:40:52 +0100

    bla

--- templates/pages/as_environment.tpl.php
+++ templates/pages/as_environment.tpl.php
@@ -1,3 +1,33 @@
+<div class="container" id="environnement">
+
+  <div class="row">
+    <div class="col s12 ">
+      <div class="  col l6 offset-l3 title">
+        <h3>Environnement</h3>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+      <div class="col l3 m4 s12 ">
+        <img class="materialboxed" width="650" id="" src="img/Environnement/_DSC0655.JPG" alt="">
+      </div>
+
+      <div class="col l3 m4 s12 ">
+        <img class="materialboxed" width="650" id="" src="img/Environnement/_DSC0428r.jpg" alt="">
+      </div>
+      <div class="col l3 m4 s12 ">
+        <img class="materialboxed" width="650" id="" src="img/Environnement/_DSC0537.JPG" alt="">
+      </div>
+      <div class="col l3 m4 s12">
+        <img class="materialboxed" width="650" id="" src="img/Environnement/_DSC0554.JPG" alt="">
+      </div>
+  </div>
+
+
+</div>
+
+
 <?php
 
 $debug->arr(array('$pageData' => $pageData));
