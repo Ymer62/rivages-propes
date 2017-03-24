15eba7c9a32a173ee5223a0c7f7dd2509f98eaea
Provain <thomas.provain@gmail.com>
2017-03-24 11:06:36 +0100

    merge fixe

--- templates/pages/as_building.tpl.php
+++ templates/pages/as_building.tpl.php
@@ -1,6 +1,4 @@
 <div class="container" id="batiment">
-<<<<<<< HEAD
-
   <div class="row">
     <div class="col s12 ">
       <div class="col l6 offset-l3 title">
@@ -23,30 +21,6 @@
       </div>
 </div>
 </div>
-
-<div class="row">
-
-      <div class="col l3 m4 s12">
-        <img class="materialboxed" width="650" id="" src="img/Bâtiment/placeholder.jpg" alt="">
-      </div>
-      <div class="col l3 m4 s12">
-        <img class="materialboxed" width="650" id="" src="img/Bâtiment/placeholder.jpg" alt="">
-      </div>
-      <div class="col l3 m4 s12">
-        <img class="materialboxed" width="650" id="" src="img/Bâtiment/placeholder.jpg" alt="">
-      </div>
-      <div class="col l3 m4 s12">
-        <img class="materialboxed" width="650" id="" src="img/Bâtiment/placeholder.jpg" alt="">
-      </div>
-=======
-    <div class="row">
-        <div class="col s12">
-            <div class="col l6 offset-l3 title">
-                <h1>Bâtiment</h1>
-            </div>
-        </div>
->>>>>>> e2bda7a306da5e7c94329b073749f392d2cfe8ff
-    </div>
 
     <div class="row">
         <div class="col s12">
--- templates/pages/as_environment.tpl.php
+++ templates/pages/as_environment.tpl.php
@@ -1,54 +1,26 @@
 <div class="container" id="environnement">
-<<<<<<< HEAD
-
-  <div class="row">
-    <div class="col s12 ">
-      <div class="  col l6 offset-l3 title">
-        <?php
-        if(ADMIN):
-        ?>
-          <h1 data-page="<?= PAGE ?>">
-            <span contenteditable="true">
-              <?php echo $pageData['title'] ?>
-            </span>
-            <i class="small material-icons editTitle">mode_edit</i>
-          </h1>
-        <?php
-        else:
-        ?>
-          <h1><?= $pageData['title'] ?></h1>
-        <?php
-        endif;
-        ?>
-      </div>
-    </div>
-  </div>
-
-  <div class="row">
-      <div class="col l3 m4 s12 ">
-        <img class="materialboxed" width="650" id="" src="img/Environnement/placeholder.jpg" alt="">
-      </div>
-
-      <div class="col l3 m4 s12 ">
-        <img class="materialboxed" width="650" id="" src="img/Environnement/placeholder.jpg" alt="">
-      </div>
-      <div class="col l3 m4 s12 ">
-        <img class="materialboxed" width="650" id="" src="img/Environnement/placeholder.jpg" alt="">
-      </div>
-      <div class="col l3 m4 s12">
-        <img class="materialboxed" width="650" id="" src="img/Environnement/placeholder.jpg" alt="">
-      </div>
-  </div>
-
-=======
     <div class="row">
         <div class="col s12">
             <div class="col l6 offset-l3 title">
-                <h1>Environnement</h1>
+              <?php
+              if(ADMIN):
+              ?>
+                <h1 data-page="<?= PAGE ?>">
+                  <span contenteditable="true">
+                    <?php echo $pageData['title'] ?>
+                  </span>
+                  <i class="small material-icons editTitle">mode_edit</i>
+                </h1>
+              <?php
+              else:
+              ?>
+                <h1><?= $pageData['title'] ?></h1>
+              <?php
+              endif;
+              ?>
             </div>
         </div>
     </div>
->>>>>>> e2bda7a306da5e7c94329b073749f392d2cfe8ff
 
     <div class="row">
         <div class="col s12">
