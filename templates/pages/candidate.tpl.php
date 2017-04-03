8399c2bb86b0c3e73ca91175a11b6d9a2535fcbb
BoillotFrederic <boillot.frederic.62@gmail.com>
2017-04-03 16:40:41 +0200

    Merge CSS Thomas

--- templates/pages/candidate.tpl.php
+++ templates/pages/candidate.tpl.php
@@ -1,4 +1,4 @@
-<div class="candidate col s8 offset-s2">
+<div id="candidate" class="col s8 offset-s2">
 
 <?php
 if(ADMIN):
@@ -17,49 +17,83 @@ else:
 endif;
 ?>
 
-<div class="candidate">
-
-<form class="col s6" action="" method="get">
-  <p>
-    <label for="prenom" >Prénom :</label>
-    <input name="prenom" id="prenom" type="text"/>
-  </p>
-    <p>
-    <label for="nom" >Nom : </label>
-    <input name="nom" id="nom" type="text"/>
-  </p>
-  <p>
-  <label for="age" class="left">Age(JJ/MM/AAAA) : </label>
-  <input name="age" id="age" type="text"/>
-</p>
-  <p>
-  <label for="mail" class="left">E-mail : </label>
-  <input name="mail" id="mail" type="email"/>
-</p>
-<p>
-<label for="cv" class="left">Piece jointe(CV) : </label>
-<input name="cv" id="cv" type="file"/>
-</p>
-
-  <p>
-    <label class="left">Votre Disponnibilité :</label>
-    <input name="lundi" id="lundi" type="checkbox" /><label for="lundi">Lundi</label>
-    <input name="mardi" id="mardi" type="checkbox" /><label for="mardi">Mardi</label>
-    <input name="mercredi" id="mercredi" type="checkbox" /><label for="mercredi">Mercredi</label>
-    <input name="jeudi" id="jeudi" type="checkbox" /><label for="jeudi">Jeudi</label>
-    <input name="vendredi" id="vendredi" type="checkbox" /><label for="vendredi">Vendredi</label>
-  </p>
-  <p>
-    <input name="matin" id="jeudi" type="checkbox" /><label for="jeudi">Matin</label>
-    <input name="apres-midi" id="vendredi" type="checkbox" /><label for="vendredi">Apres-midi</label>
-  </p>
-
-  <p>
-    <label for="commentaires" class="left">Commentaires : </label>
-    <textarea name="commentaires" id="commentaires"/></textarea>
-  </p>
-  </form>
-
+<div class="container">
+  <div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s12 m6 l6">
+          <input id="name" type="text" class="validate">
+          <label for="name">Nom</label>
+        </div>
+        <div class="input-field col s12 m6 l6">
+          <input id="lastName" type="text" class="validate">
+          <label for="lastName">Prénom</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12 m6 l6">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+        <div class="input-field col s12 m6 l6">
+          <input id="date" type="text" class="validate" placeholder="JJ/MM/YYYY">
+          <label for="date">Âge</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="file-field input-field">
+           <div class="btn">
+             <span>Votre CV</span>
+             <input type="file">
+           </div>
+           <div class="file-path-wrapper">
+             <input class="file-path validate" type="text">
+           </div>
+         </div>
+      </div>
+      <div class="row">
+        <div class="file-field input-field">
+           <div class="btn">
+             <span>Attestation CAF</span>
+             <input type="file">
+           </div>
+           <div class="file-path-wrapper">
+             <input class="file-path validate" type="text">
+           </div>
+         </div>
+       </div>
+       <div class="row">
+         <div class="col s12">
+           <?php
+           if(ADMIN):
+           ?>
+             <div class="row">
+                 <div data-page="<?= PAGE ?>" class="input-field col s12" id="boxEditText">
+                     <div data-btn="btnSubmitText" class="editor" id="first">
+                         <?= $pageData['text'] ?>
+                     </div>
+                 </div>
+             </div>
+             <div class="row">
+               <div class="col s12">
+                 <button data-box="boxEditText" id="btnSubmitText" class="btnTextAdmin waves-effect waves-light btn white-text grey darken-4 right">
+                   <i class="small material-icons right">mode_edit</i>
+                   Appliquer
+                 </button>
+               </div>
+             </div>
+           <?php
+           else:
+           ?>
+             <p class="flow-text"><?= $pageData['text'] ?></p>
+           <?php
+           endif;
+           ?>
+         </div>
+       </div>
+     </div>
+    </form>
+  </div>
 </div>
 
 <?php
