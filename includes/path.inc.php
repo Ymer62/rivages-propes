a62525581efc83a221eb74c4e76523ea14b68b0a
BoillotFrederic <boillot.frederic.62@gmail.com>
2017-04-04 10:14:13 +0200

    Design

--- includes/path.inc.php
+++ includes/path.inc.php
@@ -6,12 +6,15 @@ class path{
     $previewHome = G_preview ? '?preview=true' : '';
     $preview = G_preview ? '&preview=true' : '';
 
-    if(!$page)
-    return './' . $previewHome;
+    $designHome = isset($_GET['design']) ? '?design='.$_GET['design'] : '';
+    $design = isset($_GET['design']) ? '&design='.$_GET['design'] : '';
+
+    if(!trim($page) || $page == 'home')
+    return './' . $previewHome . $designHome;
     else if(REWRITING && !$preview)
     return (!$data ? 'form-' : '') . $page;
     else
-    return 'index.php?page=' . $page . (!$data ? '&noData=true' : '') . $preview;
+    return 'index.php?page=' . $page . (!$data ? '&noData=true' : '') . $preview . $design;
   }
 }
 
