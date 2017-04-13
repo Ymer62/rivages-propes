f59c1864a7a259cd95e4b5ae638e0fcc9eb27517
qvolant <quentin.volant@gmail.com>
2017-04-13 09:28:23 +0200

    Modif ajax pour inclure titre + supression bordures en mobile

--- ajax/showEvent.ax.php
+++ ajax/showEvent.ax.php
@@ -8,4 +8,5 @@ $id = isset($_POST['id'])? $_POST['id']:1;
 
 
 // Lorem ipsum temporaire
-echo 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+echo '<h2>Titre évènement</h2>';
+echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
